cc574196f1379574c564cb843e152aa6b1aa2a34
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2022-12-28 11:05:05 -0300

    valida dados do filme

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,10 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O Titulo do filme e obrigatório.")]
     public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Genero do filme e obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres.")]
     public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duracao deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 }
