0742e8aade3fa00f8c8df44ea01ad1e5f8db8beb
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2022-12-28 20:14:45 -0300

    adiciona Dto

---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class CreateFilmeDto
+{
+    [Required(ErrorMessage = "O Titulo do filme e obrigatório.")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Genero do filme e obrigatório")]
+    [StringLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres.")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duracao deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -5,18 +5,5 @@ namespace FilmesApi.Models;
 
 public class Filme
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-
-    [Required(ErrorMessage = "O Titulo do filme e obrigatório.")]
-    public string Titulo { get; set; }
-
-    [Required(ErrorMessage = "O Genero do filme e obrigatório")]
-    [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres.")]
-    public string Genero { get; set; }
-
-    [Required]
-    [Range(70, 600, ErrorMessage = "A duracao deve ter entre 70 e 600 minutos")]
-    public int Duracao { get; set; }
+  
 }
