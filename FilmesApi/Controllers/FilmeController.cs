c8568d285cd73fe5aced78bed3b1373341b59624
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2022-12-28 10:34:14 -0300

    adiciona controller filme

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+using System;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
